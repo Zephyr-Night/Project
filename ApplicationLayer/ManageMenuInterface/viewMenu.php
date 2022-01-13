da8e285cb2a5e5533150cf4399059d9633d02d48
AymanAziz <89571593+AymanAziz@users.noreply.github.com>
2022-01-13 21:16:05 +0800

    final change

--- ApplicationLayer/ManageMenuInterface/viewMenu.php
+++ ApplicationLayer/ManageMenuInterface/viewMenu.php
@@ -68,7 +68,6 @@ $name = $_SESSION['username'];
 		</link>
 		    <link href="/Project/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="/Project/css/home.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
 <!-- STYLE -->
 
@@ -121,7 +120,7 @@ body {
 }
 
 .status {
-  color: green;
+  color: green;/*edit*/
   font-size: 16px;
 }
 
@@ -170,6 +169,7 @@ body, html {
   background-repeat: no-repeat;
   background-size: cover;
   position: relative;
+  
 }
 
 .hero-text {
@@ -258,7 +258,6 @@ $sno = $row + 1;
         <li><a href="/Project/ApplicationLayer/ManageMenuInterface/viewMenu.php"><i class="fa fa-book"></i><span>Menu</span></a></li>
         <li><a href="/Project/ApplicationLayer/ManageOrderInterface/cart.php"><i class="fa fa-shopping-cart"></i><span>Cart</span></a></li>
         <li><a href="/Project/ApplicationLayer/ManageRefundInterface/refundList.php"><i class="fa fa-money"></i><span>Refund</span></a></li>
-        <li><a href="/Project/ApplicationLayer/ManageRefundInterface/myRefund.php"><i class="material-icons" style="font-size:21px">class</i><span>My Refund</span></a></li>
         <li><a href="/Project/ApplicationLayer/ManageCustomerInterface/logout.php" onclick="return confirm('Are you sure you want to sign out?')"><i class="fa fa-sign-out"></i><span>Sign Out</span></a></li>
                 
         <a href="/Project/ApplicationLayer/ManageCustomerInterface/customerProfile.php" id="topnav-right"><i class="fa fa-user"></i><span>Hello <?php echo $name; ?></span></a>
